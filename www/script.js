9ecc1da43fb2bf13edccceb627de8060319da3fd
buhbuh <JuggleDrum@hotmail.co.uk>
2015-01-14 00:03:03 +0000

    tweeked timings

--- www/script.js
+++ www/script.js
@@ -1,10 +1,10 @@
 document.createElement('header');
 $(function(){
     function slide() {
-        $(".slideshow div:eq(0)").animate({width:"0"}, 600, function() {
+        $(".slideshow div:eq(0)").animate({width:"0"}, 1000, function() {
             $(this).appendTo($(".slideshow")).css({width:"50%"});
         });
     }
 
-    setInterval(slide, 4000);
+    setInterval(slide, 6000);
 })
